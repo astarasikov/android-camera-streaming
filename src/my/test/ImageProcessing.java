26e9b0264e21b27532e2a97a732521a2403bf043
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-05-01 23:30:20 +0400

    Implement 2D convolution and add some test method

--- src/my/test/ImageProcessing.java
+++ src/my/test/ImageProcessing.java
@@ -1,6 +1,12 @@
 package my.test;
 
+import java.util.Arrays;
+
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.media.FaceDetector;
+import android.media.FaceDetector.Face;
 
 public class ImageProcessing {
 	public static class Kernel2D {
@@ -40,28 +46,118 @@ public class ImageProcessing {
 			}
 			return new Kernel2D(matrix, radius, radius);
 		}
+		
+		public static Kernel2D Identity() {
+			return new Kernel2D(new int[] {
+				0, 0, 0,
+				0, 1, 0,
+				0, 0, 0,
+			}, 3, 3);
+		}
+		
+		public static Kernel2D Sobel() {
+			int matrix[] = new int[] {
+				1, 2, 1,
+				0, 0, 0,
+				-1, -2, -1,
+			};
+			return new Kernel2D(matrix, 3, 3);
+		}
+		
+		public static Kernel2D Test() {
+			return new Kernel2D(new int[] {
+				1, 1, 1,
+				1, -9, 1,
+				1, 1, 1,
+			}, 3, 3);
+		}
 	}
 	
 	public static class SeparableKernel2D {
 		
 	}
 	
-	public static void Convolve2D(Kernel2D kernel, 
+	/**
+	 * Convolves the image with the filter kernel
+	 * @param kernel the convolution filter kernel as a 2d matrix
+	 * @param rgb image data; rgb.length <= width * height
+	 * @param width image width
+	 * @param height image height
+	 */
+	
+	public static void Convolve2D(Kernel2D kernel,
 			int rgb[], int width, int height)
 	{
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				int tempRed = 0;
-				int tempGreen = 0;
-				int tempBlue = 0;
+		/*
+		 * copy the old image because the result at each pixel
+		 * depends on the original value in the neighbouring pixels
+		 */
+		int old[] = Arrays.copyOf(rgb, rgb.length);
+		
+		int dx = kernel.width >> 1;
+		int dy = kernel.height >> 1;
 				
-				for (int ki = 0; ki < kernel.width; ki++) {
-					for (int kj = 0; kj < kernel.height; kj++) {
+		int mask_r = 0xff;
+		int mask_g = 0xff;
+		int mask_b = 0xff;
+		
+		int shift_r = 16;
+		int shift_g = 8;
+				
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				int red = 0;
+				int green = 0;
+				int blue = 0;
+				
+				for (int ki = 0; ki < kernel.height; ki++) {
+					/*
+					 * Flip the kernel as per definition of convolution
+					 */
+					int kern_row = kernel.height - ki - 1;
+					int img_row = i + kern_row - dy;
+					
+					/*
+					 * Wrap around edges. An alternative approach
+					 * is to pad with zeroes, but if we wrap
+					 * around, the transformation is reversible
+					 * and black edges do not appear
+					 */
+					if (img_row < 0 || img_row >= height) {
+						img_row += height;
+						img_row %= height;
+					}
+					
+					for (int kj = 0; kj < kernel.width; kj++) {
+						int kern_column = kernel.width - kj - 1;
+						int img_col = j + kern_column - dx;
+						if (img_col < 0 || img_col >= width) {
+							img_col += width;
+							img_col %= width;
+						}
 						
+						int kern_idx = kern_row * kernel.width + kern_column;
+						
+						int pixel = old[img_row * width + img_col];
+						int _red = (pixel >> shift_r) & mask_r;
+						int _green = (pixel >> shift_g) & mask_g;
+						int _blue = pixel & mask_b;
+						
+						red += _red * kernel.matrix[kern_idx];
+						green += _green * kernel.matrix[kern_idx];
+						blue += _blue * kernel.matrix[kern_idx];
 					}
 				}
 				
+				if (kernel.magnitude != 0) {
+					red = mask_r & (red / kernel.magnitude);
+					green = mask_g & (green / kernel.magnitude);
+					blue = mask_b & (blue / kernel.magnitude);
+				}
 				
+				int result = (0xff << 24) | (red << shift_r)
+						| (green << shift_g) | blue;
+				rgb[width * i + j] = result;
 			}
 		}
 	}
@@ -71,13 +167,14 @@ public class ImageProcessing {
 	{
 	}
 	
-	
 	public static void preProcess(int rgb[], int width, int height) {
 		//for (int i = 0; i < rgb.length; i++) {
 		//	rgb[i] = 0xff000000 | ~(rgb[i] & 0xffffff);
 		//}
+		//Convolve2D(Kernel2D.Identity(), rgb, width, height);
+		Convolve2D(Kernel2D.Test(), rgb, width, height);
 	}
-	public static void process(Bitmap bitmap) {
-		bitmap.setPixel(30, 30, 0xffff0000);
+	public static Bitmap process(Bitmap bitmap) {		
+		return bitmap;
 	}
 }
